eebf812388c42f7c23700d99b308de996f0502a0
Henry Steere <henry.steere@gmail.com>
2019-04-19 15:10:00 +0200

    Start on reading data

---
+++ read_data.hpp
@@ -0,0 +1,15 @@
+#ifndef _READ_DATA_H_
+#define _READ_DATA_H_
+
+#include "data.hpp"
+
+using namespace std;
+
+struct read_data {
+
+    game_state read_state(string filepath);
+    
+
+};
+
+#endif
