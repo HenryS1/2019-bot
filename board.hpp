56c33750adbc086d8c07eb4de6cbf014842d6d76
Henry Steere <henry.steere@gmail.com>
2019-04-20 10:04:05 +0200

    Use template parameter for layer size to make testing easier

--- board.hpp
+++ board.hpp
@@ -8,29 +8,25 @@
 
 using namespace std;
 
+template <uint8_t WIDTH>
 struct layer {
 
     explicit layer(const vector<vector<cell>>& map, const string& type) {
-        uint8_t row_index = 0;
-        assert(map.size() == 33);
+        assert(map.size() == WIDTH);
         for (auto row : map) {
-            assert(row.size() == 33);
-            uint64_t current_row = 0;
-            uint64_t current_position_mask = 1;
-            uint8_t col_index = 0;
+            assert(row.size() == WIDTH);
             for (auto c : row) {
-                assert(c.x == col_index);
-                assert(c.y == row_index);
+                uint8_t row_index = c.y;
+                uint64_t current_position_mask = 1ULL << c.x;
                 if (c.type == type) {
-                    current_row |= current_position_mask;
+                    rows[row_index] |= current_position_mask;
                 }
                 current_position_mask <<= 1;
             }
-            rows[row_index] = current_row;
         }
     }
 
-    uint64_t rows[33];
+    uint64_t rows[WIDTH] = {0};
 
 };
 
