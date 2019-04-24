1e69b424d44a5f0b54603d1f635ee4244be6b40a
Henry Steere <henry.steere@gmail.com>
2019-04-24 22:41:24 +0200

    Test move candidates when all adjacent blocks are open

--- tests.cpp
+++ tests.cpp
@@ -143,6 +143,25 @@ TEST(board, is_correctly_constructed_from_map) {
 
 }
 
+TEST(board, when_all_squares_are_available_all_directions_are_move_candidates) {
+    
+    uint64_t air_rows[9] = { 511, 511, 511, 511, 511, 511, 511, 511, 511 };
+    uint64_t dirt_rows[9] = {0};
+    uint64_t deep_space_rows[9] = {0};
+
+    layer<9> air(air_rows);
+    layer<9> dirt(dirt_rows);
+    layer<9> deep_space(deep_space_rows);
+
+    uint8_t damage = 4, range = 6, digging_range = 1;
+
+    board<9> b(dirt, air, deep_space, damage, range, digging_range);
+
+    b.my_worms[0] = game_worm(1, 1, 5);
+
+    ASSERT_EQ((int)b.move_candidates(b.my_worms[0]), 255);
+}
+
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
